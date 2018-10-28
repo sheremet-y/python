8c6884b9c929a5ef93a9b574f6b853146f9777b5
sheremet-y <sheremet-y@mail.ru>
2018-10-28 13:31:32 +0400

    добавлена сетка и оси для графика

--- drawman.py
+++ drawman.py
@@ -34,13 +34,57 @@ def on_vector(dx, dy):
 
 
 def to_point(x, y):
-    global x_current, y_current, _drawman_scale
+    global x_current, y_current, _drawman_scale_x, _drawman_scale_y
     x_current = x
     y_current = y
-    t.goto(_drawman_scale_x*x_current,_drawman_scale_y*y_current)
+    t.goto(_drawman_scale_x * x_current,_drawman_scale_y * y_current)
+
+def axes_y(x, y):
+    pen_up()
+    to_point(x, -y)
+    pen_down()
+    to_point(x, y)
+    pen_up()
+    to_point(x, 0)    
+
+def axes_x(x, y):
+    pen_up()
+    to_point(-x, y)
+    pen_down()
+    to_point(x, y)
+    pen_up()
+    to_point(0, y)
+    
+def grid(x_step, x_max, y_step, y_max):
+    global t, _drawman_scale_x, _drawman_scale_y
+    color_mem = t.color()
+    print(str(color_mem), type(color_mem))
+    t.color('black')
+    axes_y(0, y_max)
+    axes_x(x_max, 0)
+
+    t.color('grey')
+    i = x_step 
+    while i <= x_max:
+        axes_y(i, y_max)
+        i += x_step 
+    i = -x_step 
+    while i >= -x_max:
+        axes_y(i, y_max)
+        i -= x_step
+
+    i = y_step 
+    while i <= y_max:
+        axes_x(x_max, i)
+        i += y_step 
+    i = -y_step 
+    while i >= -y_max:
+        axes_x(x_max, i)
+        i -= y_step
+    t.color(color_mem[0])
 
 init_drawman()
 if __name__ == '__main__':
     import time
     test_drawman()
-    time.sleep(10)
+    #time.sleep(0.5)
--- task_dm3.py
+++ task_dm3.py
@@ -3,12 +3,13 @@ from drawman import *
 def f(x):
     return x*x*x
 
-drawman_scale(50, 5)
-x = -5.0
+drawman_scale(50, 8)
+x = -3.0
+grid(1.0, 3, 10.0, f(3))
 to_point(x, f(x))
 
 pen_down()
-while x < 5:
+while x < 3:
     to_point(x, f(x))
     x += 0.1
 pen_up()
