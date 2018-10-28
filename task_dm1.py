888f81df89f04e5cdaff7b2009696bb6b0005d5f
sheremet-y <sheremet-y@mail.ru>
2018-10-28 10:35:09 +0400

    изменение чертежника и рисование функции

---
+++ task_dm1.py
@@ -0,0 +1,9 @@
+from drawman import *
+
+A = [(0, 0), (100, 0), (100, 100), (0, 100)]
+
+pen_down()
+for x, y in A:
+    to_point(x, y)
+to_point(A[0][0], A[0][1])
+pen_up()
---
+++ task_dm2.py
@@ -0,0 +1,15 @@
+from drawman import *
+
+def f(x):
+    return x*x
+
+drawman_scale(50)
+x = -5.0
+to_point(x, f(x))
+
+pen_down()
+while x < 5:
+    to_point(x, f(x))
+    x += 0.1
+pen_up()
+
